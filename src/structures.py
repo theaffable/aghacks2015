99964ff17e70c20d224b5c91a0ecc7a5aee121d7
Jakub Szecówka <kuba.szecowka+work@gmail.com>
2015-10-24 01:59:34 +0200

    Added new classes to structures.py

--- src/structures.py
+++ src/structures.py
@@ -7,3 +7,29 @@ class Rectangle(object):
         self.y = y
         self.width = width
         self.height = height
+
+
+class Obstacle(object):
+    """ Class used to represent obstacles. Each of those obstacles has it's own location on X axis. """
+    def __init__(self, x):
+        self.x = x
+
+
+class Cactus(Obstacle):
+    """
+    Class that represents a cactus obstacle seen in the game. Cactus inherits X axis location from Obstacle class
+    and adds it's own field - height.
+    """
+    def __init__(self, x, height):
+        super.__init__(x)
+        self.height = height
+
+
+class Pterodactyl(Obstacle):
+    """
+    Class that represents a pterodactyl obstacle seen later in the game. Inherits X axis location from Obstacle class
+    and adds Y axis location.
+    """
+    def __init__(self, x, y):
+        super.__init__(x)
+        self.y = y
