d575508e51b028ec4b31597b2a3b6b62f92b3533
Jakub Szecówka <kuba.szecowka+work@gmail.com>
2015-10-24 01:47:23 +0200

    Modified util.py

--- src/util.py
+++ src/util.py
@@ -1,5 +1,9 @@
-""" Module containing all of the utility functions. """
+"""
+Module containing all of the utility functions.
+We will need this module only if canvas-through-websocket won't work.
+"""
 
 
 def find_game_rectangle(image):
     """ Finds game rectangle in the given screenshot. Returns rectangle structure from structures.py. """
+    pass
