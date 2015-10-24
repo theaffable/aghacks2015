dfc10614a32b7323914cc5cb3949450a6d57e1d1
Piotr Oramus <oramuspiotr2@gmail.com>
2015-10-24 02:15:07 +0200

    Added basic PIL processing

--- src/image_parser.py
+++ src/image_parser.py
@@ -1,1 +1,12 @@
 """ Module responsible for image interpretation. """
+
+import Image
+
+im = Image.open("../resources/canvas1.png")
+
+pix = im.load()
+
+for x in xrange(0, im.size[0]):
+    line = pix[x, 131]
+    if line != (83, 83, 83, 255):
+        print x, line
