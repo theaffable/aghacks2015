b8ed57d8a2d521db0c362a708a66c7f9e6be5f94
Jakub Szecówka <kuba.szecowka+work@gmail.com>
2015-10-24 10:56:27 +0200

    Removed pip warnings.

--- src/image_parser.py
+++ src/image_parser.py
@@ -14,38 +14,35 @@ import numpy as np
 path = "../resources/templates"
 templatesPaths = [f for f in listdir(path) if isfile(join(path, f))]
 
+
 def process_image(image):
-	hit = 0
-	for templatePath in templatesPaths:
-		image = np.array(image)
-		img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-		template = cv2.imread(path+"/"+templatePath, 0)
-		w, h = template.shape[::-1]
+    hit = 0
+    for templatePath in templatesPaths:
+        image = np.array(image)
+        img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        template = cv2.imread(path+"/"+templatePath, 0)
+        w, h = template.shape[::-1]
 
-		res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
-		threshold = 0.8
-		loc = np.where(res >= threshold)
-		for pt in zip(*loc[::-1]):
-			hit += 1
-			cv2.rectangle(image, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-	
-	#todo offset of trees
-	print "amount of trees:", hit
-	cv2.imshow("img", image)
+        res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
+        threshold = 0.8
+        loc = np.where(res >= threshold)
+        for pt in zip(*loc[::-1]):
+            hit += 1
+            cv2.rectangle(image, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
 
-
-
-if __name__== "__main__":
-	browser = IC.open_browser()
-	while(True):
-		image = IC.get_image_from_browser(browser)
-		process_image(image)
-		#todo rethink performing action & sleep
-		IC.perform_action(browser)
-		sleep(1)
+    # todo offset of trees
+    print "amount of trees:", hit
+    cv2.imshow("img", image)
 
 
-
+if __name__ == "__main__":
+    browser = IC.open_browser()
+    while True:
+        image = IC.get_image_from_browser(browser)
+        process_image(image)
+        # todo rethink performing action & sleep
+        IC.perform_action(browser)
+        sleep(1)
 
 
 def test():
